60cbec05fcd0169cecddb03c7b1d73ca493d7eb1
sardarali <sardarali785621@gmail.com>
2023-08-07 20:47:13 +0000

    first project commit

---
+++ src/test/java/demo/seleniumDemo/Demo.java
@@ -0,0 +1,19 @@
+package demo.seleniumDemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		
+	}
+
+}
